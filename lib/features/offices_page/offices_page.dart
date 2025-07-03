94b8519fe3d215dabfb821c705d6b68ead8bb357
Mohamad Ali Alnuaimi <Mohamad Ali Alnuaimi@NGP>
2025-07-03 16:22:20 +0300

    edit

--- lib/features/offices_page/offices_page.dart
+++ lib/features/offices_page/offices_page.dart
@@ -25,44 +25,51 @@ class OfficesPage extends GetView<OfficesController> {
             const AppBarSearch(title: "المكاتب"),
             const AdsSliderWidget(),
             TopOffice(controller: controller),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: AppPadding.p14,
-                  ),
-                  child: Text(
-                    "كل المكاتب",
-                    style: Get.textTheme.headlineMedium!.copyWith(
-                      color: ColorManager.secColor,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: AppSize.s18),
-                SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: List.generate(controller.topOffices.length, (
-                      index,
-                    ) {
-                      final item = controller.topOffices[index];
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 6,
-                          horizontal: AppPadding.p14,
-                        ),
-                        child: OfficeCardStyle2(model: item),
-                      );
-                    }),
-                  ),
-                ),
-                const SizedBox(height: AppSize.s16),
-              ],
-            ),
+            AllOffices(controller: controller),
           ],
         ),
       ),
+    );
+  }
+}
+
+class AllOffices extends StatelessWidget {
+  const AllOffices({super.key, required this.controller});
+
+  final OfficesController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: AppPadding.p14),
+          child: Text(
+            "كل المكاتب",
+            style: Get.textTheme.headlineMedium!.copyWith(
+              color: ColorManager.secColor,
+            ),
+          ),
+        ),
+        const SizedBox(height: AppSize.s18),
+        SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: List.generate(controller.topOffices.length, (index) {
+              final item = controller.topOffices[index];
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 6,
+                  horizontal: AppPadding.p14,
+                ),
+                child: OfficeCardStyle2(model: item),
+              );
+            }),
+          ),
+        ),
+        const SizedBox(height: AppSize.s16),
+      ],
     );
   }
 }
