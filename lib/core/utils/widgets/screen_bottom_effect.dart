c808c0a8ce81aaa33f9aa3b52d6f30f5026e059d
Osama <osamazerkawi1@gmail.com>
2025-07-10 02:29:28 +0300

    add screen bottom effect

---
+++ lib/core/utils/widgets/screen_bottom_effect.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:property_ms/core/utils/color_manager.dart';
+class ScreenBottomEffect extends StatelessWidget {
+  const ScreenBottomEffect({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      left: 0,
+      right: 0,
+      child: IgnorePointer(
+        child: Container(
+          height: Get.context!.height * 0.2,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.transparent,
+                ColorManager.primaryColor.withAlpha((0.3 * 255).round()),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
