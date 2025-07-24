f502ef4d88f4fbd230d4b00088dd6c57b59e0a79
Mohamad Ali Alnuaimi <Mohamad Ali Alnuaimi@NGP>
2025-07-24 15:22:02 +0300

    d

--- lib/core/utils/widgets/custom_cached_network_image_widget.dart
+++ lib/core/utils/widgets/custom_cached_network_image_widget.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:property_ms/core/utils/assets.gen.dart';
+
 import 'package:property_ms/core/utils/color_manager.dart';
 import 'package:property_ms/core/utils/values_manager.dart';
 import 'package:shimmer/shimmer.dart';
@@ -71,34 +71,34 @@ class _DefaultPlaceholder extends StatelessWidget {
   }
 }
 
-class _DefaultErrorWidget extends StatelessWidget {
-  final double width;
-  final double height;
-  final double borderRadius;
+// class _DefaultErrorWidget extends StatelessWidget {
+//   final double width;
+//   final double height;
+//   final double borderRadius;
 
-  const _DefaultErrorWidget({
-    this.width = AppSize.s60,
-    this.height = AppSize.s60,
-    this.borderRadius = AppSize.s8,
-  });
+//   const _DefaultErrorWidget({
+//     this.width = AppSize.s60,
+//     this.height = AppSize.s60,
+//     this.borderRadius = AppSize.s8,
+//   });
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      decoration: BoxDecoration(
-        color: ColorManager.grey2,
-        borderRadius: BorderRadius.circular(borderRadius),
-      ),
-      child: Assets.icons.imageIcon.svg(
-        width: width,
-        height: height,
-        colorFilter: const ColorFilter.mode(
-          ColorManager.grey3,
-          BlendMode.srcIn,
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: width,
+//       height: height,
+//       decoration: BoxDecoration(
+//         color: ColorManager.grey2,
+//         borderRadius: BorderRadius.circular(borderRadius),
+//       ),
+//       child: Assets.icons.imageIcon.svg(
+//         width: width,
+//         height: height,
+//         colorFilter: const ColorFilter.mode(
+//           ColorManager.grey3,
+//           BlendMode.srcIn,
+//         ),
+//       ),
+//     );
+//   }
+// }
